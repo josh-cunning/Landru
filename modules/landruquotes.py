22fefe078fc8d3faa33355309e5c65ae0f9d27f7
Joshua Cunningham <josh@gmscpa.com>
2019-10-22 14:46:26 -0700

    landru

---
+++ modules/landruquotes.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals, absolute_import, print_function, division
+
+import random
+import re
+import operator
+
+import sopel.module
+from sopel.tools.calculation import eval_equation
+
+@sopel.module.commands("landru")
+def qoutes(bot, trigger):
+    bot.say("Landru is always watching you.")
