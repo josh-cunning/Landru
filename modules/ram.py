00f010fba781a89a345ccccc28001e5549bb5273
Joshua Cunningham <josh@gmscpa.com>
2019-10-23 11:39:46 -0700

    ram

--- modules/ram.py
+++ modules/ram.py
@@ -12,10 +12,9 @@ from sopel.tools.calculation import eval_equation
 
 @sopel.module.commands("ram")
 def moarram(bot, trigger):
-    answer = ["More RAM is better","Please insert 5 quarts of blood for moar ram", "It's my ram and you can't have any"," I checked and we don't care what you want"]
+    answer = ["More RAM is better", "Please insert 5 quarts of blood for moar ram", "It's my ram and you can't have any"," I checked and we don't care what you want"]
     max = len(answer)
     txt = random.randint(0, max)
     bot.say(str(txt))
-    quote = answer(txt)
-    # txt = "Get your own dam ram"
+    quote = answer[txt]    
     bot.say(quote)
