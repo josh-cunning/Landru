d6549cfbf1a4269b155c401dd0b4c5434292c23d
Joshua Cunningham <josh@gmscpa.com>
2019-10-23 13:27:38 -0700

    ram

--- modules/landruquotes.py
+++ modules/landruquotes.py
@@ -15,6 +15,5 @@ def qoutes(bot, trigger):
         answer = ["More RAM is better", "Please insert 5 quarts of blood for moar ram", "It's my ram and you can't have any"," I checked and we don't care what you want"]
         max = len(answer)
         txt = random.randint(0, max)
-        bot.say(str(txt))
         quote = answer[txt]
         bot.say(quote)
