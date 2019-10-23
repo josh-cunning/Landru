ae7dbab46234c8d33debf83a259d23fd4275b8d2
Joshua Cunningham <josh@gmscpa.com>
2019-10-23 11:08:51 -0700

    ram

--- modules/ram.py
+++ modules/ram.py
@@ -15,6 +15,7 @@ def moarram(bot, trigger):
     answer = ["More RAM is better","Please insert 5 quarts of blood for moar ram", "It's my ram and you can't have any"," I checked and we don't care what you want"]
     max = len(answer)
     txt = random.randint(0, max)
+    bot.say(str(text))
     quote = answer(txt)
     # txt = "Get your own dam ram"
     bot.say(quote)
