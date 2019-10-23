e12a7ae3b33da320450d8ccb7804aa5c079fc7fc
Joshua Cunningham <josh@gmscpa.com>
2019-10-23 13:47:15 -0700

    ra

--- modules/ram.py
+++ modules/ram.py
@@ -7,7 +7,7 @@ import sopel.module
 import operator
 import sopel.module
 from sopel.tools.calculation import eval_equation
-from ramchips import *
+from sopel.modules.ramchips import *
 
 @sopel.module.commands("ram")
 def moarram(bot, trigger):
