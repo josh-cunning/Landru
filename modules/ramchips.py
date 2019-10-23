9458807d58adb0b44f96ddcbe8283d51bfd816cf
Joshua Cunningham <josh@gmscpa.com>
2019-10-23 13:01:18 -0700

    moar

---
+++ modules/ramchips.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals, absolute_import, print_function, division
+
+import random
+import re
+import operator
+
+import sopel.module
+import operator
+
+import sopel.module
+from sopel.tools.calculation import eval_equation
+
+def getbalance(bot,nick):
+    bl=0
+    bl = bot.db.get_nick_value(nick, 'rambank')
+    return bl
+
+def addram(bot,nick,amount):
+     bl = 0
+     bl = bot.db.get_nick_value(nick, 'rambank') + amount
+     bot.db.set_nick_value(nick,'rambank',bl)
