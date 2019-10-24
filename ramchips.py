071fae0446249f2e77dfebbb078eee30881433f0
Joshua Cunningham <josh@gmscpa.com>
2019-10-24 16:29:14 -0700

    chips

--- ramchips.py
+++ ramchips.py
@@ -11,10 +11,10 @@ import sopel.module
 
 def getbalance(bot,nick):
     bl=0
-    bl = int(bot.db.get_nick_value(nick, 'rambank'))
+    bl = bot.db.get_nick_value(nick, 'rambank')
     return bl
 
 def addram(bot,nick,amount):
      bl = 0
-     bl = int(bot.db.get_nick_value(nick, 'rambank')) + amount
+     bl = bot.db.get_nick_value(nick, 'rambank') + amount
      bot.db.set_nick_value(nick,'rambank',bl)
