7ee5f114d2918a3d51956973df82b4667f10cb47
Joshua Cunningham <josh@gmscpa.com>
2019-10-24 12:52:24 -0700

    r

--- modules/ram.py
+++ modules/ram.py
@@ -14,14 +14,15 @@ from ramchips import *
 @sopel.module.commands("ram")
 def moarram(bot, trigger):
     coms = trigger.group(2)
-    bot.say(str(coms))
+    #bot.say(str(coms))
     nick = trigger.nick
     if coms == 'add':
         amount = 1
         addram(bot,nick,amount)
         bot.say(nick + " gets " + str(amount) + " ramchips" )
     elif coms == 'balance':
+        bl = 0
         bl = getbalance(bot,nick)
-        bot.say(str(bl))
+        bot.say(nick + " has " + str(bl) + " RAM chips")
     else:
         bot.say("Get your own ram")
