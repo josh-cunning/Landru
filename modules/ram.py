209e5bc10a608f30c658d40d85959541741562bd
Joshua Cunningham <josh@gmscpa.com>
2019-10-31 15:09:47 -0700

    ra

--- modules/ram.py
+++ modules/ram.py
@@ -36,7 +36,7 @@ def moarram(bot, trigger):
     else:
 
         if coms == 'add':
-            if bot.channels[trigger.sender].privileges[bot.nick] < sopel.module.HALFOP:
+            if bot.channels[trigger.sender].privileges[nick] < sopel.module.HALFOP:
                 return bot.reply("I'm not a channel operator!")
             target = spicemanip.main(triggerargsarray,2) or 0
             amount = spicemanip.main(triggerargsarray,3)
