6ba31df3f0c1e2bb869609d0fd06b0dc22c79b50
Joshua Cunningham <josh@gmscpa.com>
2020-03-19 14:44:41 -0700

    restart control

---
+++ controller.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals, absolute_import, print_function, division
+
+import random
+import re
+import operator
+import sopel.module
+import operator
+import sopel.module
+import os
+import sys
+import landrushared
+
+import sopel.module
+from sopel.tools.calculation import eval_equation
+
+
+@sopel.module.commands("restart")
+def restartbot(bot, trigger):
+    if coms == 'setbots':
+        if bot.channels[trigger.sender].privileges[nick] < sopel.module.HALFOP:
+            return bot.reply("You are not a channel operator!")
+        bot.say("Restarting")
+        landrushared.restart(bot,trigger)
